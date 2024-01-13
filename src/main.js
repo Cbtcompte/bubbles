a1832c9cc1d573e5083894b5ac7206a49eedb98d
Michael DENAHOU <michaeldenahou@gmail.com>
2024-01-14 00:37:57 +0100

    fini sans la partie connexion

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-// import piniaPersist from 'pinia-plugin-persist'
 
 import App from './App.vue'
 import router from './router'
